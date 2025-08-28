3a7df185612ddcc75d5bc505e4cba99582d2fe37
Konstantin Dzyuba <konstantin.dzyuba.2003@gmail.com>
2025-08-29 00:11:56 +0300

    tilemap transmission

--- src/Client.java
+++ src/Client.java
@@ -1,12 +1,24 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) {
+    private static final int TILE_SIZE = 64;
+    private static final Tile[][] tilemap = new Tile[100][100];
+    private static BufferedImage image;
+
+    public static void main(String[] args) throws IOException {
+        image = ImageIO.read(Client.class.getResourceAsStream("grass.png"));
+        openLogInFrame();
+//        openMainFrame();
+    }
+
+    private static void openLogInFrame() {
         JFrame logInFrame = new JFrame("Legends of Grind");
         logInFrame.setLocationRelativeTo(null);
         logInFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,8 +45,11 @@ public class Client {
                 Socket socket = new Socket("localhost", 52);
                 OutputStream outputStream = socket.getOutputStream();
                 outputStream.write("login".getBytes());
+                outputStream.write('\n');
                 outputStream.write(logInLogin.getText().getBytes());
+                outputStream.write('\n');
                 outputStream.write(logInPassword.getText().getBytes());
+                outputStream.write('\n');
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -55,10 +70,8 @@ public class Client {
                 outputStream.write(nickname.getText().getBytes());
                 outputStream.write('\n');
                 if (inputStream.read() == 1) {
-                    JFrame frame = new JFrame("Legends of Grind");
-                    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    frame.setVisible(true);
+                    receiveTilemap(inputStream);
+                    openMainFrame();
                     logInFrame.dispose();
                 }
             } catch (IOException e) {
@@ -68,5 +81,37 @@ public class Client {
         logInFrame.add(registerButton);
         logInFrame.pack();
         logInFrame.setVisible(true);
+    }
+
+    private static void receiveTilemap(InputStream inputStream) throws IOException {
+        for (int i = 0; i < tilemap.length; i++) {
+            for (int j = 0; j < tilemap.length; j++) {
+                tilemap[i][j] = Tile.values()[inputStream.read()];
+            }
+        }
+        System.out.println("Successfully received tilemap from server: " + tilemap);
+    }
+
+    private static void openMainFrame() {
+        JFrame frame = new JFrame("Legends of Grind");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(image, 0, 0, TILE_SIZE, TILE_SIZE, null);
+            }
+        };
+        frame.add(panel);
+        frame.setVisible(true);
+        try {
+            while (true) {
+                panel.repaint();
+                Thread.sleep(20);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,13 +1,20 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Server {
+    private static final Path path = Path.of("tilemap.txt");
+    private static final Tile[][] tilemap = new Tile[100][100];
+
     public static void main(String[] args) throws IOException, SQLException {
+        loadOrGenerateTilemap();
         ServerSocket serverSocket = new ServerSocket(52);
         Socket socket = serverSocket.accept();
         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -24,8 +31,58 @@ public class Server {
             preparedStatement.setString(3, nickname);
             preparedStatement.execute();
             outputStream.write(1);
+            sendTilemap(outputStream);
         } else if (operation.equals("login")) {
 
         } else throw new IllegalStateException();
+    }
+
+    private static void sendTilemap(OutputStream outputStream) throws IOException {
+        for (int i = 0; i < tilemap.length; i++) {
+            for (int j = 0; j < tilemap.length; j++) {
+                outputStream.write(tilemap[i][j].ordinal());
+            }
+        }
+        System.out.println("Successfully sent tilemap to client");
+    }
+
+    private static void loadOrGenerateTilemap() {
+        if (Files.exists(path)) {
+            loadTilemap();
+        } else {
+            generateTilemap();
+        }
+    }
+
+    private static void loadTilemap() {
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path)) {
+            for (int i = 0; i < tilemap.length; i++) {
+                for (int j = 0; j < tilemap.length; j++) {
+                    tilemap[i][j] = Tile.values()[bufferedReader.read() - '0'];
+                }
+                bufferedReader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void generateTilemap() {
+        for (int i = 0; i < tilemap.length; i++) {
+            for (int j = 0; j < tilemap.length; j++) {
+                tilemap[i][j] = Tile.GRASS;
+            }
+        }
+    }
+
+    private static void saveTilemap() throws IOException {
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path, StandardOpenOption.CREATE)) {
+            for (int i = 0; i < tilemap.length; i++) {
+                for (int j = 0; j < tilemap.length; j++) {
+                    bufferedWriter.write(tilemap[i][j].ordinal() + '0');
+                }
+                bufferedWriter.write('\n');
+            }
+        }
     }
 }
